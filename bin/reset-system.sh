e7a90a2f104f9a027d3242b1de708669a7af5442
Bhaskar Tiwari <bt@qbuzzer.com>
2021-03-19 14:29:15 +0530

    Added -m Switch for Max

--- bin/reset-system.sh
+++ bin/reset-system.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-brightness 0.5
-osascript -e "set Volume 4"
+while getopts m flag; do
+  case "${flag}" in
+  m) MAX=1 ;;
+  esac
+done
+
+if [[ "$MAX" == 1 ]]; then
+  brightness 1
+  osascript -e "set Volume 10"
+else
+  brightness 0.5
+  osascript -e "set Volume 4"
+fi
